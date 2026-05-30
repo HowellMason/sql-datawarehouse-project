1de672eb028cc7075104b62d804ebef2038ffa99
Mason Howell <124301276+HowellMason@users.noreply.github.com>
2026-05-30 17:39:07 -0500

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -15,6 +15,7 @@ Script Purpose:
 
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
+
 CREATE TABLE silver.crm_cust_info (
 	cst_id INT,
 	cst_key NVARCHAR(50),
@@ -30,6 +31,7 @@ CREATE TABLE silver.crm_cust_info (
 
 IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
+
 CREATE TABLE silver.crm_prd_info (
 	prd_id INT,
 	prd_key NVARCHAR(50),
@@ -45,6 +47,7 @@ CREATE TABLE silver.crm_prd_info (
 
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
+
 CREATE TABLE silver.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
@@ -65,6 +68,7 @@ CREATE TABLE silver.crm_sales_details (
 
 IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
+
 CREATE TABLE silver.erp_cust_az12 (
 	cid NVARCHAR(50),
 	bdate DATE,
@@ -76,6 +80,7 @@ CREATE TABLE silver.erp_cust_az12 (
 
 IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
+
 CREATE TABLE silver.erp_loc_a101(
 	cid NVARCHAR(50),
 	cntry NVARCHAR(50),
@@ -86,6 +91,7 @@ CREATE TABLE silver.erp_loc_a101(
 
 IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2;
+
 CREATE TABLE silver.erp_px_cat_g1v2(
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
